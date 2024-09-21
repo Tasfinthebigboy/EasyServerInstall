88fa6120220c03d69397abd6f15da6ef65e5ddd0
Tasfinthebigboy <96976817+Tasfinthebigboy@users.noreply.github.com>
2024-09-21 20:32:32 +0600

    Update start.bat

--- 1.20.1/start.bat
+++ 1.20.1/start.bat
@@ -4,6 +4,13 @@ echo container@pterodactyl~ Server marked as running...
 echo Hashes differ. Downloading requirements
 
 if not exist plugins mkdir plugins
+if not exist eula.txt (
+    (
+        echo # By changing the setting below to TRUE you are indicating your agreement to our EULA ^(https://aka.ms/MinecraftEULA^).
+        echo # %date% %time%
+        echo eula=true
+    ) > eula.txt
+)
 if not exist server.jar curl -o server.jar https://api.papermc.io/v2/projects/paper/versions/1.20.1/builds/196/downloads/paper-1.20.1-196.jar
 if not exist plugins\ViaVersion.jar curl -o plugins\ViaVersion.jar https://basichtml.ignorelist.com/ViaVersion-5.0.1.jar
 if not exist plugins\ViaBackwards.jar curl -o plugins\ViaBackwards.jar https://basichtml.ignorelist.com/ViaBackwards-5.0.1.jar
