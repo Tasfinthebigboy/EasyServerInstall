528589b9ca71756736f28fa27c349ef12c4e1846
Tasfinthebigboy <96976817+Tasfinthebigboy@users.noreply.github.com>
2024-09-21 22:03:39 +0600

    Update start.bat

--- 1.20.4/start.bat
+++ 1.20.4/start.bat
@@ -1,5 +1,25 @@
 @echo off
 
+echo [Daemon]: Checking server disk space usage, this could take a few seconds...
+
+for /f "tokens=3" %%a in ('dir C: ^| find "bytes free"') do (
+    set freeSpace=%%a
+)
+
+set freeSpace=%freeSpace:,=%
+set /a freeSpaceMB=freeSpace/1024/1024  :: Convert bytes to MB
+
+echo [Daemon]: Free space available: %freeSpaceMB% MB
+
+if %freeSpaceMB% lss 200 (
+    echo [Error]: Insufficient disk space. At least 200 MB is required.
+    exit /b 1
+) else (
+    echo [Daemon]: Disk space is sufficient.
+)
+
+echo [Daemon]: Updating process configuration files...
+echo [Daemon]: Ensuring file permissions are set correctly, this could take a few seconds...
 echo container@pterodactyl~ Server marked as running...
 echo Hashes differ. Downloading requirements
 if not exist eula.txt (
@@ -14,10 +34,14 @@ if not exist server.jar curl -o server.jar https://api.papermc.io/v2/projects/pa
 if not exist plugins\ViaVersion.jar curl -o plugins\ViaVersion.jar https://basichtml.ignorelist.com/ViaVersion-5.0.1.jar
 if not exist plugins\ViaBackwards.jar curl -o plugins\ViaBackwards.jar https://basichtml.ignorelist.com/ViaBackwards-5.0.1.jar
 if not exist server.properties curl -o server.properties https://basichtml.ignorelist.com/server.properties
+
+echo container@pterodactyl~ Server marked as starting or online...
+echo [Daemon]: Pulling Docker container image, this could take a few minutes to complete...
+echo [Daemon]: Finished pulling Docker container image
+
 echo container@pterodactyl~ java -version
 java -version
 echo container@pterodactyl~ java -Xmx3156M -Xms3156M -jar server.jar nogui
-echo container@pterodactyl~ Server marked as starting or online...
 java -Xmx3156M -Xms3156M -jar server.jar nogui
 echo container@pterodactyl~ Server marked as offline.
 pause
